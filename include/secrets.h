f4a65d6547751ea289e13b7dd3aeb578e3424151
Karbomusic <6774981+karbomusic@users.noreply.github.com>
2022-04-03 12:53:09 -0400

    Initial commit

---
+++ include/secrets.h
@@ -0,0 +1,17 @@
+/*+===================================================================
+  File:      secrets.h
+
+  Summary:   Wifi SSID and Credentials.
+
+  Kary Wall 2/26/2022.
+===================================================================+*/
+
+#include <Arduino.h>
+
+#ifndef STASSID
+#define STASSID "IOTNET2G"
+#define STAPSK "creekvalley124"
+#endif
+
+String ssid = STASSID;                         // WiFi ssid
+String password = STAPSK;                      // WiFi password
